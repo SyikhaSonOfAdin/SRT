e850ab2c2399658eb903d804350bf26c9028383b
SyikhaSonOfAdin <syikhasmart@gmail.com>
2024-10-02 09:14:11 +0700

    Update v1.2.7

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -124,7 +124,7 @@ class Security {
               uId: this.encrypt(user[TABLES.USER.COLUMN.ID]), // User Id
               uName: user[TABLES.USER.COLUMN.USERNAME], // Username
               jwtToken: jwt.sign({ email: user[TABLES.USER.COLUMN.EMAIL] }, process.env.SECRET_KEY, { expiresIn: '7d' }),
-              version: "1.2.6",
+              version: "1.2.7",
               privileges: privileges, // User Privileges
             };
           } else {
